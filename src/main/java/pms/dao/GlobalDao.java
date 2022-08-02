da9969a161403d9e497c66066faff822d3d3e2a5
IDontHaveBrain <skawns848@naver.com>
2022-08-02 15:44:37 +0900

    대시보드 임시 로그인 처리

--- src/main/java/pms/dao/GlobalDao.java
+++ src/main/java/pms/dao/GlobalDao.java
@@ -1,8 +1,9 @@
 package pms.dao;
 // pms.dao.GlobalDao
 import org.springframework.stereotype.Repository;
+import pms.vo.Member;
 
 @Repository
 public interface GlobalDao {
-
+    public Member getMember(String email);
 }
